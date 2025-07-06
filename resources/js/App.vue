f3e538e2aba79fdd47f739922550c15e85a514d7
MaxGrateman <grekf2@mail.ru>
2025-07-06 18:10:54 +0500

    Add routing & tailwind config

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="container mx-auto py-10">
-    <h1 class="text-2xl font-semibold mb-4">Testing</h1>
-    <div class="text-gray-500">Testing testing.</div>
-    <div class="mt-4 bg-gray-100 p-4 rounded">
-      <p>Email: testing@example.com</p>
-      <p>Status: idle</p>
-    </div>
-  </div>
+  <router-view/>
 </template>
 
 <script setup lang="ts">
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.{vue,js,ts,jsx,tsx,blade.php}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
